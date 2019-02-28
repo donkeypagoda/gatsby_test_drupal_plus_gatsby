71c13c46e0e913c3644b6967246e2f480e3638e2
Christopher Stewart <christopherallenstewart@gmail.com>
2019-02-28 11:04:52 -0800

    got images rendering from drupal

--- src/pages/tea.js
+++ src/pages/tea.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { graphql } from "gatsby"
-
+import { graphql } from "gatsby";
+import Img from 'gatsby-image';
 
 const TeaPage = ({data}) => (
   <div>
@@ -9,6 +9,9 @@ const TeaPage = ({data}) => (
       <div>
         <h3>{node.title}</h3>
         <div dangerouslySetInnerHTML={{ __html: node.body.value}} />
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Img alt="tea" fluid={node.relationships.field_tea_image.localFile.childImageSharp.fluid} />
+        </div>
       </div>
     ))}
   </div>
@@ -27,6 +30,17 @@ export const query  = graphql`
             format
             processed
             summary
+          }
+          relationships{
+            field_tea_image {
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 400, quality: 100) {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
           }
         }
       }
