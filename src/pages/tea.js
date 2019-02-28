65632913d7836588dcbb6a2b96aac7b252d84714
Christopher Stewart <christopherallenstewart@gmail.com>
2019-02-27 17:37:22 -0800

    borked my brain for the day

--- src/pages/tea.js
+++ src/pages/tea.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { graphql } from "gatsby"
 
-const TeaPage = () => (
+
+const TeaPage = ({data}) => (
   <div>
     <h1>Different types of tea</h1>
+    {data}
   </div>
 )
 export default TeaPage;
+
+export const query  = graphql`
+{
+  nodeTea {
+    id
+    title
+    body {
+      value
+      format
+      processed
+      summary
+    }
+    relationships{
+      field_tea_image {
+        id
+      }
+    }
+
+  }
+}
+`
