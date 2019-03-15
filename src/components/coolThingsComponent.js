d5954613031f85584ce09e4de62a0a6d1a1d00fc
Christopher Stewart <christopherallenstewart@gmail.com>
2019-03-15 13:05:30 -0700

    stable, stopping

--- src/components/coolThingsComponent.js
+++ src/components/coolThingsComponent.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import Img from 'gatsby-image';
+import styles from '../tea.module.css'
 
 
 function Coolness(props){
@@ -8,8 +9,8 @@ function Coolness(props){
         {props.data.allNodeCoolThings.edges.map(({node}) => (
           <div>
             <h3>{node.title}</h3>
-            <div className="teaPics" style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-              <Img alt="tea" fluid={node.relationships.field_cool_things_image.localFile.childImageSharp.fluid} />
+            <div className={styles.teaPics} style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+              <Img alt="cool" fluid={node.relationships.field_cool_things_image.localFile.childImageSharp.fluid} />
             </div>
             <div dangerouslySetInnerHTML={{ __html: node.body.value}} />
           </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styles from '../tea.module.css'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Base" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Waassup!</h1>
+    <h1>Hayo</h1>
     <p>Welcome to my test of Gatsby with de-coupled Drupal.</p>
+    <div className={styles.teaPics} style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <Image />
+    </div>
     <div>
       <Link to="/tea/">Go to Tea page</Link>
     </div>
